023c55d90123ab86e6fcf8711670a29e05ad046a
vishvas-choudhary <vishvaschoudhary7@gmail.com>
2021-02-19 23:45:09 +0530

    hero section added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
 // console.log("Hello World!");
+
+const hamburger = document.querySelector(".hamburger");
+const links = document.querySelector(".hamburger-links");
+const social = document.querySelector(".hamburger-social");
+const overlay = document.querySelector(".overlay");
+
+hamburger.addEventListener("click", () => {
+  social.classList.toggle("fade-in");
+  overlay.classList.toggle("fade-in");
+  links.classList.toggle("fade-in");
+});
